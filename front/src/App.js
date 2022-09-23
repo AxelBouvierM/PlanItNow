06c92bd9d71c77b7e1cb1c271224e9c8f6d057b5
MatiasMtz <4442@holbertonschool.com>
2022-09-23 14:15:53 +0000

    updated node version 18.9.0

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,25 @@
-import React, { useState, useEffect } from 'react'
-// useState: Contains the data retrieved from the backend & renders the data on the page
-// useEffect: Fetchs the backend Api on the first render
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-  const [data, setData] = useState([{}])
-  // data: Actual data
-  // setData: Function to manipulate data variable
-
-  useEffect(() => {
-    fetch("ruta de flask")
-    .then(res => res.json())
-    .then(data => {
-      setData(data)
-      console.log(data)
-    })
-  }, [])
-  // empty array at end to run just one time
   return (
-    <div>
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>): (
-          data.members.map((member, i) => (
-            <p key={i}>{member}</p>
-          ))
-        )
-      }
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
