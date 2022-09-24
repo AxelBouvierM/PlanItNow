913f29ac9771c1fae98ef6f51c3f1f4e647b7991
MatiasMtz <4442@holbertonschool.com>
2022-09-24 18:21:45 +0000

    hamburger menu & fixes

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,10 +1,8 @@
 import { useRef } from 'react';
 
+import { Fade as Hamburger } from 'hamburger-react'
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { VscSettingsGear } from 'react-icons/vsc';
-import { HiOutlineUserGroup } from "react-icons/hi";
-import { BsCalendarRange } from 'react-icons/bs';
-import { RiHome2Line, RiHistoryFill, RiAccountCircleLine } from 'react-icons/ri';
+import { RiHome2Line, RiAccountCircleLine, RiCalendar2Line, RiGroup2Line, RiSettings2Line, RiHistoryLine } from 'react-icons/ri';
 
 import '../styles/navigation.css';
 
@@ -17,19 +15,16 @@ function Navbar() {
   	return (
 	<header>
 		<div className="btn-container">
-			<button type="button" className='nav-btn' onClick={showNavbar}>
-				<FaBars />
+			<button type="button" id="openButton" className='nav-btn' onClick={showNavbar}>
+				<Hamburger />
 			</button>
 			<nav ref={navRef}>
 				<a href='/#'><i><RiHome2Line /></i><span>Inicio</span></a>
-				<a href='/#'><i><BsCalendarRange /></i><span>Agenda</span></a>
-				<a href='/#'><i><HiOutlineUserGroup /></i><span>Sala</span></a>
-				<a href='/#'><i><RiHistoryFill /></i><span>Historial</span></a>
+				<a href='/#'><i><RiCalendar2Line /></i><span>Agenda</span></a>
+				<a href='/#'><i><RiGroup2Line /></i><span>Sala</span></a>
+				<a href='/#'><i><RiHistoryLine /></i><span>Historial</span></a>
 				<a href='/#'><i><RiAccountCircleLine /></i><span>Cuenta</span></a>
-				<a href='/#'><i><VscSettingsGear /></i><span>Configuración</span></a>
-				<button className='nav-btn nav-close-btn' onClick={showNavbar}>
-					<FaTimes/>
-				</button>
+				<a href='/#'><i><RiSettings2Line /></i><span>Configuración</span></a>
 			</nav>
 		</div>
 	</header>
