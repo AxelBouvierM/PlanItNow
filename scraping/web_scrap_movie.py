9f1e56c647482ac7d36c92063d9eb818e37869f3
MaxiHBTN <4387@holbertonschool.com>
2022-09-26 17:44:34 +0000

    Add description information

--- scraping/web_scrap_movie.py
+++ scraping/web_scrap_movie.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from ast import Attribute
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
@@ -20,7 +21,6 @@ for movie_element in movie_elements:
     counter += 1
     title = str(movie_element.h2.a).split(">")[1].split("<")[0]
     image = movie_element.img['src']
-    link = movie_element.a['href']
     link = "https://www.movie.com.uy/" + movie_element.a['href'][1:]
     p_list = movie_element.find_all('div', class_="col-lg-6 col-md-6 col-sm-6 col-xs-6")
     duration = p_list[0].text.replace("\n","").split("Duración")[1]
@@ -28,6 +28,18 @@ for movie_element in movie_elements:
     genre = p_list[2].text.replace("\n","").split("Género")[1]
     actors = p_list[3].text.replace("\n","").split("Actores")[1]
     locations = movie_element.ul.text.replace("\n","").replace("Movie", " Movie").replace("VER", " VER")[1:]
+    driver.get(link)
+    lxml = driver.page_source
+    soup = BeautifulSoup(lxml, 'lxml')
+    html = str(soup.contents[0])
+    soup = BeautifulSoup(html, 'lxml')
+    description = soup.find("div", class_="colapse").contents[1].text
+    if description == "":
+        try:
+            description = soup.find("div", class_="colapse open").contents[1].text
+        except AttributeError:
+            print("Start again")
+            break
     print(f"{counter}-Title: {title}")
     print(f"Image: {image}")
     print(f"Link: {link}")
@@ -36,4 +48,5 @@ for movie_element in movie_elements:
     print(f"Duration: {duration}")
     print(f"Actors: {actors}")
     print(f"Locations: {locations}")
+    print(f"Description: {description}")
     
