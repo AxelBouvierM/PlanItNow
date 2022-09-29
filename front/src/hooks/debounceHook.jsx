37e32488e9cad9fd4ccbfe94e93b9e9ef5a88589
MatiasMtz <4442@holbertonschool.com>
2022-09-29 03:16:39 +0000

    searchbar components

---
+++ front/src/hooks/debounceHook.jsx
@@ -0,0 +1,19 @@
+import { useCallback, useEffect } from "react";
+import { useState } from "react";
+
+export function useDebounce(value, timeout, callback) {
+	const [timer, setTimer] = useState(null);
+
+	const clearTimer = useCallback(() => {
+		if (timer) clearTimeout(timer);
+	}, [timer]);
+
+	useEffect(() => {
+		clearTimer();
+
+		if (value && callback) {
+			const newTimer = setTimeout(callback, timeout);
+			setTimer(newTimer);
+		}
+	}, [value, clearTimer, callback, timeout]);
+}
