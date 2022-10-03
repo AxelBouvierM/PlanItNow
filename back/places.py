e347470e3195708602ea7e8d203a13c68c196690
MaxiHBTN <4387@holbertonschool.com>
2022-10-03 23:43:39 +0000

    First version of loading restaurant data

--- back/places.py
+++ back/places.py
@@ -1,14 +1,65 @@
 #!/usr/bin/python3
 
-from googleplaces import GooglePlaces, types, lang
 import requests
-from pprint import pprint
+import json
+import pprint
 
 API_KEY = open('API_KEY.txt').read() #open and save the api keyinto a variable
-google_places = GooglePlaces(API_KEY) #creates a google places instance
+import requests
 
-result = google_places.nearby_search(location='Montevideo Uruguay', keyword='Restaurants', radius=1000, types=[types.TYPE_RESTAURANT])
-for place in result.places:
-    print("--------------------------")
-    print (place.__dict__)
-    break
+url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query=Restaurantes%20en%20Montevideo%20Uruguay&key={API_KEY}"
+
+response = requests.get(url)
+results = response.json()
+places = results.get('results')
+
+for place in places:
+      
+    # Print value corresponding to the
+    # 'name' key at the ith index of y
+    photo_reference = place.get('photos')[0].get('photo_reference')
+    
+    title = place.get('name')
+    place_location = place.get('formatted_address')
+    price_level = place.get('price_level') #Make a scale
+    price = "Rango de precios: " 
+    if price_level == 0:
+        price += "Gratis"
+    elif price_level == 1:
+        price += "Sin precio"
+    elif price_level == 2:
+        price += "Moderado"
+    elif price_level == 2:
+        price += "Costoso"    
+    else:
+        price += "Muy costoso"
+    place_id = place.get('place_id')
+    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&key={API_KEY}"
+    response = requests.get(url)
+    results = response.json().get('result')
+    phone_number = results.get('international_phone_number') #Description
+    open_days = results.get('opening_hours').get('weekday_text') #Description
+    description = f"Telefono: {phone_number}\n"
+    for day in open_days:
+        description += day + "\n"
+    link = results.get('website')
+    image = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference={photo_reference}&key={API_KEY}"
+
+    print("===============================")
+    print(title)
+    print(image)
+    print(link)
+    print(place_location)
+    print(price)
+    print(description[:-1])
+    print("===============================")
+
+"""
+title 
+image 
+link 
+place 
+date
+price
+description
+"""
