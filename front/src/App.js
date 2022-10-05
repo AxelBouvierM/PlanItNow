ecb17ecf4b0bb1ff711bf8760b38850eb5cc0ab2
ezesilva95 <4381@holbertonschool.com>
2022-10-05 12:18:40 +0000

    update

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 import React from 'react';
 import { NavBar } from './components/header/NavBar';
 import { SearchBar } from './components/body/searchBar/SearchBar';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import { dataDigitalBestSeller } from './data';
+
 
 
 const AppContainer = styled.div`
@@ -14,6 +19,40 @@ const AppContainer = styled.div`
 `;
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div>
       <React.Fragment>
@@ -22,6 +61,21 @@ function App() {
       <AppContainer>
         <SearchBar />
       </AppContainer>
+      <Slider {...settings}>
+        {dataDigitalBestSeller.map((item) =>(
+          <div className='card'>
+            <div className='card-top'>
+              <img src={item.linkImg} alt={item.title} />
+              <h1>{item.title}</h1>
+            </div>
+            <div className='card-bottom'>
+              <h3>{item.price}</h3>
+              <span className="category">{item.category}</span>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    
     </div>
   );
 }
