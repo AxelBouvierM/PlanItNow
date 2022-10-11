39a6ca979eadff2ffa7d7d3f193a25c42f5bb6a6
MatiasMtz <4442@holbertonschool.com>
2022-10-11 20:35:57 +0000

    landpage header implementation

--- front/src/pages/Landpage.js
+++ front/src/pages/Landpage.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import Register from './Register'
-import Login from './Login'
+import styled from 'styled-components'
+
+import LandPageHeader from '../components/LandingPageHeader/LandPageHeader'
+
+import montaña5 from '../images/montaña5.jpg'
+
+
+const Background = styled.div`
+  border: 1px solid #000; 
+  background-image: url(${montaña5});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function Landpage() {
 
 	// crear botones para login y register con un modulo link y outlet de react-router-dom
   return (
-	<div>
-		<p>PAGINA LANDING</p>
-	</div>
+	<Background>
+	  <LandPageHeader />
+	</Background>
   )
 }
 
