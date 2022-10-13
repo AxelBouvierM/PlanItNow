c8bcff3e1b3140f3fe04a23f98a2d0ce3220a867
MaxiHBTN <4387@holbertonschool.com>
2022-10-13 20:21:50 +0000

    Script to run monthly

---
+++ back/monthly_data_loading.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+
+python3 /home/planitnow_pin/PlanItNow/back/RESTAURANT_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/COFFEE_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/ENTERTAINMENT_load_dat.py
+python3 /home/planitnow_pin/PlanItNow/back/BREWERY_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/MUSEUM_load_data.py
