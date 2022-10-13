9f74992cca092f6eb6feba4fe3c316ec976a1bb4
ezesilva95 <4381@holbertonschool.com>
2022-10-13 16:11:47 +0000

    update

--- front/src/pages/Categories.js
+++ front/src/pages/Categories.js
@@ -1,24 +1,41 @@
 import styled from 'styled-components';
 import React from 'react';
 import montaña2 from '../images/montaña2.jpg'
+import CatSlides from '../components/CategoriesSlider/CatSlides';
+import { Parallax, Background } from 'react-parallax';
 
+const bgStyles = {
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover'
+};
 
-const Background = styled.div`
-  border: 1px solid #000; 
-  background-image: url(${montaña2});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 100vw;
-  height: 100vh;
+const TitleCat = styled.h1`
+  position: relative;
+  top: 100px;
+  text-align: center;
+  font-size: 50px;
+  color: #a1a1a1;
+  text-shadow: 0 0 8px black;
 `;
+
 
 function Categories() {
   return (
-    <Background>
-      <h1>PAGINA CATEGORIAS</h1>
-    </Background>
-  )
+    <>
+      <Parallax strength={500}>
+        <Background className='customBg' bgImageStyle={bgStyles}>
+          <img src={montaña2} alt='montaña2' />
+        </Background>
+        <div className='bgDimensions' style={{ height: '100vh' }}>
+          <div>
+            <TitleCat>Juegos</TitleCat>
+            <CatSlides />
+          </div>
+        </div>
+      </Parallax>
+    </>
+  );
 }
 
 export default Categories;
