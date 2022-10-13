62913f6ef28b0da1957719e161c0a5c9399f337e
MaxiHBTN <4387@holbertonschool.com>
2022-10-13 20:57:55 +0000

    Script to run daily

---
+++ back/daily_data_loading.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash 
+
+python3 /home/planitnow_pin/PlanItNow/back/MOVIE_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/TA_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/RT_load_dat.py
