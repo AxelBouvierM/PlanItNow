2e20eee00150bc0e0075621c63d3db6e9bfcba30
MaxiHBTN <4387@holbertonschool.com>
2022-10-17 18:44:15 +0000

    Scripts

--- back/daily_data_loading.sh
+++ back/daily_data_loading.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-cat /home/planitnow_pin/PlanItNow/back/delete_daily_data.sql | mysql -hlocalhost -uroot -p
+cat /home/planitnow_pin/PlanItNow/back/delete_daily_data.sql | mysql -hlocalhost -uroot -pAEPINMM
 python3 /home/planitnow_pin/PlanItNow/back/MOVIE_load_data.py
 python3 /home/planitnow_pin/PlanItNow/back/TA_load_data.py
 python3 /home/planitnow_pin/PlanItNow/back/RT_load_data.py
--- back/monthly_data_loading.sh
+++ back/monthly_data_loading.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
 
-cat /home/planitnow_pin/PlanItNow/back/delete_monthly_data.sql | mysql -hlocalhost -uroot -p
+cat /home/planitnow_pin/PlanItNow/back/delete_monthly_data.sql | mysql -hlocalhost -uroot -pAEPINMM
 python3 /home/planitnow_pin/PlanItNow/back/RESTAURANT_load_data.py
 python3 /home/planitnow_pin/PlanItNow/back/COFFEE_load_data.py
 python3 /home/planitnow_pin/PlanItNow/back/ENTERTAINMENT_load_dat.py
