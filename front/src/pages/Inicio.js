a02289afc017216d5743364bd72144f17107d801
MatiasMtz <4442@holbertonschool.com>
2022-10-17 11:43:19 +0000

    home page update

--- front/src/pages/Inicio.js
+++ front/src/pages/Inicio.js
@@ -1,31 +1,69 @@
 import React from 'react'
-import { Parallax, Background } from 'react-parallax'
+import styled from 'styled-components';
+import { Parallax } from 'react-parallax';
+
 
 import { NavBar } from '../components/header/NavBar'
 import { SearchBar } from '../components/body/searchBar/SearchBar'
 import Slides from '../components/body/Carousel/Slides';
 
-import image1 from '../images/montaña1.jpg'
-import '../styles/slides.css'
+import rambla from '../images/rambla6.jpg'
+import waves from '../images/layered-waves-haikei.svg'
+import Footer from '../components/footer/Footer';
+import { FaFileExcel } from 'react-icons/fa';
 
 
-const searchBarStyles = {
-  display: 'flex',
-  justifyContent: 'center',
-  marginTop: '10rem',
-  zIndex: '2'
-};
+const TopSectionContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: block;
+  position: relative;
+`;
+
+const Background = styled.div`
+  display: flex;
+  position: relative;
+  background-image: url(${rambla});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+`;
+
+const Content = styled.div`
+  display: block;
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const PhraseContainer = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  margin-top: 1.5em;
+  justify-content: center;
+`;
+
+const Phrase = styled.p`
+  margin-top: 1em;
+	color: white;
+`;
 
 const navBarStyles = {
-  position: 'fixed',
+  position: 'relative',
+  display: 'flex',
+  width: '100%',
   top: '1%',
   zIndex: '3'
 };
 
-const bgStyles = {
-  backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover'
+const searchBarStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '1.5em',
+  zIndex: '2',
 };
 
 function Inicio() {
@@ -33,7 +71,35 @@ function Inicio() {
   // a landpage
   return (
     <>
-      <Parallax strength={500}>
+    <TopSectionContainer>
+      <Background>
+          <Content>
+            <div className='navBar' style={navBarStyles}>
+              <NavBar />
+            </div>
+            <PhraseContainer>
+              <Phrase>- Dejá que Montevideo te guíe -</Phrase>
+            </PhraseContainer>
+            <div className='searchBar' style={searchBarStyles}>
+              <SearchBar />
+            </div>
+            <div>
+              <Slides />
+            </div>
+          </Content>
+      </Background>
+      <div>
+        <Footer />
+      </div>
+    </TopSectionContainer>
+
+    </>
+  );
+}
+
+export default Inicio;
+
+/* <Parallax strength={500}>
         <Background className='customBg' bgImageStyle={bgStyles}>
           <img src={image1} alt='montaña' />
         </Background>
@@ -50,9 +116,4 @@ function Inicio() {
             </div>
           </div>
         </div>
-      </Parallax>
-    </>
-  );
-}
-
-export default Inicio;
+      </Parallax> */
