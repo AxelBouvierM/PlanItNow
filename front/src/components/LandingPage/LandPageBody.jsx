67d251a9e5b089adb4fee16a245bacbdfa2dafe6
MatiasMtz <4442@holbertonschool.com>
2022-10-17 20:29:29 +0000

    landing changed

--- front/src/components/LandingPage/LandPageBody.jsx
+++ front/src/components/LandingPage/LandPageBody.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-
+import React from 'react';
 import styled from 'styled-components';
 
-import { IconContext } from "react-icons";
-import { RiSearchEyeLine, RiPushpinLine, RiCalendarCheckLine } from 'react-icons/ri'
 
 const BodyContainer = styled.div`
 	display: flex;
@@ -48,21 +45,6 @@ const Phrase = styled.p`
 	padding: 0 1em 0 1em;
 `;
 
-const ImageContainer = styled.div`
-	display: flex;
-	position: relative;
-	width: 60%;
-	height: 100%;
-`;
-
-const DecImage = styled.img`
-	display: flex;
-	position: relative;
-	width: 100%;
-	height: auto;
-	padding: 2em;
-`;
-
 const SecondBodyContainer = styled.div`
 	display: flex;
 	position: relative;
@@ -70,122 +52,6 @@ const SecondBodyContainer = styled.div`
 	height: 100vh;
 	align-items: center;
 	flex-direction: column;
-`;
-
-// cambiar a display block cuando el display sea menor a 600px
-const OfferContainer = styled.div`
-	display: flex;
-	position: relative;
-	width: 100%;
-	height: 30%;
-	background-color: #2E2E2E;
-
-`;
-
-const DiscoverContainer = styled.div`
-	display: flex;
-	position: relative;
-	align-items: center;
-	justify-content: center;
-	width: 33%;
-	height: 100%;
-	&:hover {
-  		transition: 0.2s ease-in-out;
-		background-color: rgba(0,0,0,0.20);
-	}
-`;
-
-const ContainerScale = styled.div`
-	display: flex;
-	position: relative;
-	width: 90%;
-	height: 90%;
-	&:hover {
-  		transition: 0.2s ease-in-out;
-		transform: scale(1.1);
-		overflow: hidden;
-	}
-`;
-
-const Discover = styled.div`
-	display: block;
-	position: relative;
-	width: 100%;
-	height: 100%;
-	&:hover {
-  		transition: 0.2s ease-in-out;
-	}
-`;
-
-const DiscoverIconContainer = styled.div`
-	display: flex;
-	position: relative;
-	width: 100%;
-	height: 60%;
-	margin-top: 1em;
-	justify-content: center;
-`;
-
-const DiscoverIcon = styled.span`
-	width: 5.5em;
-	height: 5.5em;
-	display: flex;
-	position: relative;
-	border: solid 3px #fafafa;
-	border-radius: 50%;
-	margin: auto;
-	text-align: center;
-	justify-content: center;
-	&:hover {
-		background-color: #FAA10E;
-  		transition: 0.2s ease-in-out;
-	}
-`;
-
-const Icon = styled.i`
-	margin: auto;
-`;
-
-const DiscoverTextContainer = styled.div`
-	display: flex;
-	position: relative;
-	width: 100%;
-	height: 30%;
-	align-items: center;
-	justify-content: center;
-	padding-bottom: 2em;
-`;
-
-const DiscoverText = styled.p`
-	color: #fafafa;
-	font-size: 1.2em;
-	text-transform: uppercase;
-`;
-
-const ScheduleContainer = styled.div`
-	display: flex;
-	position: relative;
-	width: 33.3%;
-	height: 100%;
-	align-items: center;
-	justify-content: center;
-	&:hover {
-		background-color: rgba(0,0,0,0.20);
-  		transition: 0.2s ease-in-out;
-	}
-`;
-
-const AnotherOffer = styled.div`
-	display: flex;
-	position: relative;
-	width: 33.3%;
-	height: 100%;
-	align-items: center;
-	justify-content: center;
-	&:hover {
-		background-color: rgba(0,0,0,0.20);
-  		transition: 0.2s ease-in-out;
-	}
 `;
 
 function LandPageBody() {
@@ -199,79 +65,8 @@ function LandPageBody() {
 					  <Phrase>Únete ahora!</Phrase>
 			  </PhraseContainer>
 		  </PhraseWrapper>
-		  <ImageContainer>
-			  <DecImage />
-		  </ImageContainer>
 	</BodyContainer>
 	<SecondBodyContainer>
-		<OfferContainer>
-			<DiscoverContainer>
-				<ContainerScale>
-					<Discover>
-							<DiscoverIconContainer>
-								<DiscoverIcon>
-									<Icon>
-										<IconContext.Provider value={{
-										color: '#fafafa',
-										className: 'enter',
-										size: '3em'
-										}}>
-											<RiSearchEyeLine />
-										</IconContext.Provider>
-									</Icon>
-								</DiscoverIcon>
-							</DiscoverIconContainer>
-							<DiscoverTextContainer>
-								<DiscoverText>Descubre lugares</DiscoverText>
-							</DiscoverTextContainer>
-					</Discover>
-				</ContainerScale>
-			</DiscoverContainer>
-			<ScheduleContainer>
-				<ContainerScale>
-					<Discover>
-							<DiscoverIconContainer>
-								<DiscoverIcon>
-									<Icon>
-										<IconContext.Provider value={{
-											color: '#fafafa',
-											className: 'enter',
-											size: '3em'
-											}}>
-											<RiPushpinLine />
-										</IconContext.Provider>
-									</Icon>
-								</DiscoverIcon>
-							</DiscoverIconContainer>
-							<DiscoverTextContainer>
-								<DiscoverText>Elige tu evento</DiscoverText>
-							</DiscoverTextContainer>
-					</Discover>
-				</ContainerScale>
-			</ScheduleContainer>
-			<AnotherOffer>
-				<ContainerScale>
-					  <Discover>
-						  	<DiscoverIconContainer>
-									<DiscoverIcon>
-										<Icon>
-											<IconContext.Provider value={{
-												color: '#fafafa',
-												className: 'enter',
-												size: '3em'
-											}}>
-												<RiCalendarCheckLine />
-											</IconContext.Provider>
-										</Icon>
-									</DiscoverIcon>
-							  </DiscoverIconContainer>
-							  <DiscoverTextContainer>
-							  	<DiscoverText>Agenda tu fecha </DiscoverText>
-							  </DiscoverTextContainer>
-					  </Discover>
-				</ContainerScale>
-			</AnotherOffer>
-		</OfferContainer>
 	</SecondBodyContainer>
 	</>
   )
