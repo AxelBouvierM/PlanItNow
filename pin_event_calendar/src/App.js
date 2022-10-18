d9cfb9bf1c9eb75aa5b88a64d3d1a4406c3b87e2
MaxiHBTN <4387@holbertonschool.com>
2022-10-18 02:09:24 +0000

    Calendar beta version

---
+++ pin_event_calendar/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import EventCalendar from './component/EventCalendar';
+
+function App() {
+  return (
+    <EventCalendar/>
+  );
+}
+
+export default App;
