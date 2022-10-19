e6454836a19c48a8aeccc7a93e5bb10258d5a578
MatiasMtz <4442@holbertonschool.com>
2022-10-19 20:03:57 +0000

    deletion

--- data/monthly_data_loading.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash 
-
-cat /home/planitnow_pin/PlanItNow/back/delete_monthly_data.sql | mysql -hlocalhost -uroot -p
-python3 /home/planitnow_pin/PlanItNow/back/RESTAURANT_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/COFFEE_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/ENTERTAINMENT_load_dat.py
-python3 /home/planitnow_pin/PlanItNow/back/BREWERY_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/MUSEUM_load_data.py
-rm -rf /tmp/.com.google.Chrome.*
