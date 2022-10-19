e87c4d494e7ad02d526b47993ab06b569218883a
MaxiHBTN <4387@holbertonschool.com>
2022-10-19 20:58:18 +0000

    Update path

--- back/data_generation/monthly_data_loading.sh
+++ back/data_generation/monthly_data_loading.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash 
 
-cat /home/planitnow_pin/PlanItNow/back/delete_monthly_data.sql | mysql -hlocalhost -uroot -pAEPINMM
-python3 /home/planitnow_pin/PlanItNow/back/RESTAURANT_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/COFFEE_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/ENTERTAINMENT_load_dat.py
-python3 /home/planitnow_pin/PlanItNow/back/BREWERY_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/MUSEUM_load_data.py
+cat /home/planitnow_pin/PlanItNow/back/database/delete_monthly_data.sql | mysql -hlocalhost -uroot -pAEPINMM
+python3 /home/planitnow_pin/PlanItNow/back/data_generation/RESTAURANT_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/data_generation/COFFEE_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/data_generation/ENTERTAINMENT_load_dat.py
+python3 /home/planitnow_pin/PlanItNow/back/data_generation/BREWERY_load_data.py
+python3 /home/planitnow_pin/PlanItNow/back/data_generation/MUSEUM_load_data.py
 rm -rf /tmp/.com.google.Chrome.*
