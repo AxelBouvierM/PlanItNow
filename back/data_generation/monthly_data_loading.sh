afdc39a15f3fb2605207db31bf674ac4ffa35505
MatiasMtz <4442@holbertonschool.com>
2022-10-21 19:46:20 +0000

    delete

--- back/data_generation/monthly_data_loading.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash 
-
-cat /home/planitnow_pin/PlanItNow/back/database/delete_monthly_data.sql | mysql -hlocalhost -uroot -pAEPINMM
-python3 /home/planitnow_pin/PlanItNow/back/data_generation/RESTAURANT_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/data_generation/COFFEE_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/data_generation/ENTERTAINMENT_load_dat.py
-python3 /home/planitnow_pin/PlanItNow/back/data_generation/BREWERY_load_data.py
-python3 /home/planitnow_pin/PlanItNow/back/data_generation/MUSEUM_load_data.py
-rm -rf /tmp/.com.google.Chrome.*
--- front/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Routes from './Routes';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Routes />);
