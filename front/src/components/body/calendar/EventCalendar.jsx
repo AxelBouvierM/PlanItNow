7f577cbf59a6d44d98ce1807ebd3f2e5d0809615
MatiasMtz <4442@holbertonschool.com>
2022-10-23 10:23:13 +0000

    get data and styles

--- front/src/components/body/calendar/EventCalendar.jsx
+++ front/src/components/body/calendar/EventCalendar.jsx
@@ -1,19 +1,60 @@
-import React from "react";
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import esLocale from '@fullcalendar/core/locales/es'
+import {React, useState} from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import axios from 'axios';
 
 function EventCalendar() {
-        return(
-            <FullCalendar
-            locale={esLocale}
-            plugins={[ dayGridPlugin ]} 
+    const [data, setData] = useState([]);
+
+    axios.get('/calendar')
+        .then((res) => {
+            console.log(res);
+            const values = Object.values(res.data)
+            setData(values)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    return (
+        <>
+        <FullCalendar
+            plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
             events={[
-                { title: 'Maria Becerra', date: '2022-10-18'},
-                { title: 'Avatar - Estreno', date: '2022-10-21'}
+                { title: 'event 1', date: '2022-10-24' },
+                { title: 'event 2', date: '2022-11-04' }
             ]}
+            headerToolbar={{
+                right: "prev,next today",
+                left: "title",
+            }}
+            titleFormat={{ 
+                month: "long",
+                year: "numeric",
+            }}
+            buttonText={{
+                today: 'Hoy',
+                month: 'Mes',
+                week: 'Semana',
+                day: 'Día',
+                list: 'Lista'
+            }}
+            height="90vh"
+            locale="es"
+            /*select={handleAddEventSelectAndOpenModal}
+            eventClick={handleEditEventSelectAndOpenModal}
+            eventChange={handleUpdateEventSelect}*/
+            initialEvents={data}
+            longPressDelay={1000}
+            eventLongPressDelay={1000}
+            selectLongPressDelay={1000}
+            dayMaxEvents={true}
+            allDaySlot={false}
+            editable={true}
             />
+        </>
         )
-}
+
+    }
 export default EventCalendar
