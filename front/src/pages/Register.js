44740113c2980d69359d8b39835af7a27d4664cb
MatiasMtz <4442@holbertonschool.com>
2022-10-23 01:30:05 +0000

    new register design

--- front/src/pages/Register.js
+++ front/src/pages/Register.js
@@ -1,21 +1,19 @@
 import { React, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { Outlet, Link, useNavigate} from 'react-router-dom';
 
-import montaña4 from '../images/montaña4.jpg'
-
+import registerBg from '../images/registerBg.jpg'
 import { IconContext } from "react-icons";
-import { RiArrowLeftLine, RiArrowRightLine, RiErrorWarningLine } from 'react-icons/ri';
+import { RiArrowLeftLine, RiArrowRightLine, RiErrorWarningLine, RiCheckLine } from 'react-icons/ri';
 import MoonLoader from 'react-spinners/MoonLoader';
 
 const Background = styled.div`
   border: 1px solid #000; 
-  background-image: url(${montaña4});
-  background-position: center;
+  background-image: url(${registerBg});
   background-repeat: no-repeat;
-  background-size: cover;
-  width: 100vw;
+    background-size: cover;
+      width: 100vw;
   height: 100vh;
 `;
 
@@ -31,27 +29,23 @@ const InfoContainer = styled.div`
   height: 100%;
 `;
 
-const GoBackButton = styled.button`
-  width: 6em;
-  height: 2.5em;
-  display: flex;
-  align-items: center;
-  position: absolute;
-  margin-top: 2em;
-  margin-left: 2em;
-  gap: 0.5em;
-  border: none;
-  border-bottom: solid 3px;
-  border-color: #fafafa;
-  color: #fafafa;
-  background-color: transparent;
-  box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.20);
-  backdrop-filter: blur(15px);
-  justify-content: center;
+const GoBackButton = styled.a`
+  display:inline-block;
+  margin: 3rem 3rem;
+  padding:0.35em 1.2em;
+  border:0.1em solid #FFFFFF;
+  border-radius:0.12em;
+  box-sizing: border-box;
+  text-decoration:none;
+  font-family:'Roboto',sans-serif;
+  font-size: 0.9em;
+  font-weight:350;
+  color:#FFFFFF;
+  transition: all 0.2s;
   cursor: pointer;
   &:hover {
-	background-color: crimson;
-  transition: 0.3s ease-in-out;
+    color:#000000;
+    background-color: #fafafa;
   }
 `;
 
@@ -88,7 +82,7 @@ const FormContainer = styled.form`
 
 const InputContainer = styled.div`
   width: 60%;
-  display: flex;
+  display: block;
   position: relative;
   align-items: center;
   justify-content: center;
@@ -119,6 +113,26 @@ const Input = styled.input`
   }
 `;
 
+const PassReq = styled.label`
+  display: block;
+  width: 100%;
+  font-size: 0.9em;
+  font-weight: 400;
+  color: crimson;
+  margin: 1em;
+  text-align: center;
+`;
+
+const Registered = styled.label`
+  display: block;
+  width: 100%;
+  font-size: 0.9em;
+  font-weight: 400;
+  color: green;
+  margin: 1em;
+  text-align: center;
+`;
+
 const DataContainer = styled.div`
   width: 100%;
   display: inline;
@@ -129,39 +143,38 @@ const DataContainer = styled.div`
 `;
 
 const ButtonStyle = styled.button`
-  width: 2.5em;
-  height: 2.5em;
-  display: inline-block;
-  align-items: center;
-  justify-content: center;
-  position: relative;
+  display:inline-block;
+  margin: 1em 1em;
+  padding:0.35em 1.2em;
+  border:0.1em solid #FFFFFF;
+  border-radius:0.12em;
+  box-sizing: border-box;
+  text-decoration:none;
+  font-family:'Roboto',sans-serif;
+  font-weight:350;
+  color:#FFFFFF;
   background-color: transparent;
-  border-radius: 20px;
-  border: none;
-  border-color: #fafafa;
-  color: white;
-  font-size: 16px;
-  vertical-align: middle;
-  margin-right: 1.5em;
+  transition: all 0.2s;
   cursor: pointer;
   &:hover {
-	background-color: crimson;
-  transition: 0.3s ease-in-out;
+    color:#000000;
+    background-color: #fafafa;
   }
 `;
 
 const Icon = styled.i`
   vertical-align: middle;
-  margin: 0 0.4em;
+  margin: 0.4em;
 `;
 
 const LoginText = styled.a`
-  color: #fafafa;
+  color: #D8D8D8;
   display: inline-block;
   vertical-align: middle;
   font-size: 0.8em;
+  transition: 0.3s ease-in-out;
   &:hover {
-	color: crimson;
+	color: white;
   transition: 0.3s ease-in-out;
   }
 `;
@@ -176,38 +189,22 @@ const LoadingWrapper = styled.div`
 `;
 
 const ErrorWrapper = styled.div`
-  width: 55%;
+  width: 50%;
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 20px;
   margin-bottom: 2em;
-  background-color: rgba(230,0,0,0.8);
-
+  background-color: rgba(230,0,0,0.5);
+  text-align: center;
 `;
 
 const ErrorMessage = styled.p`
-  font-size: 1em;
-  font-weight: 400;
+  font-size: 0.9em;
+  font-weight: 350;
   color: white;
+  padding: 0.3em;
   text-transform: uppercase;
-`;
-
-const ReqContainer = styled.div`
-  display: inline-block;
-  font-size: 1em;
-  font-weight: 350;
-  color: white;
-  margin-bottom: 0.8em;
-`;
-
-const PassRequirement = styled.div`
-  display: inline-block;
-  font-size: 0.8em;
-  font-weight: 350;
-  color: white;
-  margin-bottom: 0.8em;
 `;
 
 function Register() {
@@ -215,31 +212,36 @@ function Register() {
 	const [mail, setMail] = useState('')
 	const [password, setPass] = useState('')
   const [isLoading, setLoading] = useState(false);
-  const [LoginRedirect, setLoginRedirect] = useState(false)
   const [Error, setError] = useState('');
-
-  const [numberCheck, setNumbersCheck] = useState(false);
-  const [upperCheck, setUpperCheck] = useState(false);
-  const [lengthCheck, setLengthCheck] = useState(false);
-  const [passDetails, showPassDetails] = useState(false);
+  const [passError, setPassError] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const [nicePassword, setNicePassword] = useState(false);
+  const [correctRegister, setCorrectRegister] = useState(false);
 
   const navigate = useNavigate();
 
-  function checkForNumbers(string){
-    const matches = string.match(/\d+/g);
-    if (matches != null) setNumbersCheck(true);
-  };
+  function checkPassRequirements(string) {
+    const intMatch = string.match(/\d+/g);
+    const upperMatch = string.match(/[A-Z]/);
 
-  function checkForUpperCase(string){
-    const matches = string.match(/[A-Z]/);
-    if (matches != null) setUpperCheck(true);
-  };
+    if (string.length >= 6 && intMatch != null && upperMatch != null) {
+      setPassError(null);
+      setNicePassword(true);
+    } else if (string.length < 6) { 
+      setPassError('La contraseña debe tener al menos 6 caracteres.');
+      setNicePassword(false);
+    } else if (intMatch == null) {
+      setPassError('La contraseña debe tener al menos 1 número.');
+      setNicePassword(false);
+    } else if (upperMatch == null) {
+      setPassError('La contraseña debe tener al menos 1 mayúscula.');
+      setNicePassword(false);
+    } else {
+      setPassError('Ha ocurrido un error, vuelve a intentarlo');
+    }
+  }
 
-  function checkForLength(string) {
-    if (string.length >= 6) setLengthCheck(true)
-  };
-
-	async function SendFormInput(event) {
+  async function SendFormInput(event) {
 		event.preventDefault()
 		const headers = {
 			'Content-Type': 'application/json',
@@ -253,42 +255,38 @@ function Register() {
 		};
 
     setLoading(true);
-    console.log('Cargando?', isLoading);
 
-    const res = await axios.post('/register', regData, { headers: headers }).catch((err) => {
-      console.log("Error: ", err);
+    const res = await axios.post('/register', regData, { headers: headers })
+    .catch((err) => {
+        console.log(err);
     });
 
     if (res.data.response.status === 'Ok') {
-      // crear msj de Cuenta creada correctamente y redireccion segundos despues.
-      setLoginRedirect(true);
+      setCorrectRegister(true);
+      setTimeout(() => setRedirect(true), 1500)
+
     } else if (res.data.response.status === 'User already exists') {
       setError('Este nombre de usuario ya esta en uso');
     } else if (res.data.response.status === 'Mail already exists') {
-      setLoginRedirect(false);
       setError('Ya existe una cuenta asociada con este correo');
-    }  else {
+    } else if (res.data.response.status === 'Invalid user') {
+      setError('El nombre de usuario debe tener al menos 6 caracteres');
+    } else if (res.data.response.status === 'Please complete all the data') {
+      setError('La contraseña no cumple los requisitos');
+    } else {
       setError('Ha ocurrido un error, vuelve a intentarlo');
     }
     setLoading(false);
   };
 
-  const onClickButton = () => {
-    if (passDetails) {
-      showPassDetails(false)
-    } else {
-      showPassDetails(true)
-    }
-  }
-
-  if (LoginRedirect) navigate('/entrar');
+  if (redirect) navigate('/ingresar');
 
 	return (
 		<Background>
       <PageContainer>
         <InfoContainer>
           <Link to="/">
-            <GoBackButton type="button"><RiArrowLeftLine />Volver</GoBackButton>
+            <GoBackButton type="button"><Icon style={{ verticalAlign: 'middle', marginBottom: '0.2em' }}><RiArrowLeftLine /></Icon>Volver</GoBackButton>
           </Link>
           <Outlet />
         </InfoContainer>
@@ -323,30 +321,21 @@ function Register() {
               type="password"
               placeholder="Contraseña"
               onChange={(event) => {
-                checkForNumbers(event.target.value);
-                checkForUpperCase(event.target.value);
-                checkForLength(event.target.value);
-                if (numberCheck && upperCheck && lengthCheck) setPass(event.target.value)
+                checkPassRequirements(event.target.value)
+                if (nicePassword) setPass(event.target.value)
                 }}
               required>
               </Input>
-              <input type='button' onClick={onClickButton}></input>
             </InputContainer>
-            {passDetails && (
-              <ReqContainer>
-                <PassRequirement>
-                  <p>La contraseña debe tener al menos 6 caracteres, 1 número y 1 mayúscula.</p>
-                </PassRequirement>
-              </ReqContainer>
-              )}
+            {passError && !correctRegister && (<PassReq><Icon><RiErrorWarningLine /></Icon>{passError}</PassReq>)}
+            {correctRegister && <Registered><Icon><RiCheckLine /></Icon>Te has registrado correctamente!</Registered>}
             <DataContainer>
               <ButtonStyle type="submit">
                 <Icon>
                   <IconContext.Provider value={{
                     style: { verticalAlign: 'middle' },
-                    color: '#fafafa',
                     className: 'enter',
-                    size: '2em'
+                    size: '1.4em'
                   }}>
                     <RiArrowRightLine />
                   </IconContext.Provider>
