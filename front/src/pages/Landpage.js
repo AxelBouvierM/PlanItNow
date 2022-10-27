3f7f4b6b9d63edf8cc9a574188ad932231494502
MatiasMtz <4442@holbertonschool.com>
2022-10-27 14:35:11 +0000

    landpage

--- front/src/pages/Landpage.js
+++ front/src/pages/Landpage.js
@@ -1,20 +1,22 @@
 import { React, useEffect } from 'react';
 import styled from 'styled-components';
+import {motion} from 'framer-motion';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-import LandPageHeader from '../components/LandingPage/LandPageHeader';
-import LandPageBody from '../components/LandingPage/LandPageBody1';
+import LandPageBody1 from '../components/LandingPage/LandPageBody1';
 import LandPageBody2 from '../components/LandingPage/LandPageBody2';
 import LandPageBody3 from '../components/LandingPage/LandPageBody3';
-import LandPageBody4 from '../components/LandingPage/LandPageBody4';
+import {Footer} from '../components/footer/Footer';
 
-import { Footer } from '../components/footer/Footer';
-import landpageBg1 from '../images/landpageBg1.jpg';
-import landpageBg2 from '../images/landpageBg2.jpg';
+
 import landpageBg3 from '../images/landpageBg3.jpg';
-import landpageBg4 from '../images/landpageBg4.jpg';
-import landPageBg5 from '../images/landpageBg5.jpg';
+import landPageBg4 from '../images/landpageBg4.jpg';
+
+const LandingContainer = styled.div`
+  display: block;
+  position: relative;
+`;
 
 const BgContainer = styled.div`
   display: flex;
@@ -25,33 +27,38 @@ const BgContainer = styled.div`
 
 const Bg1 = styled.img`
   display: flex;
-  position: relative;
+  position: absolute;
   width: 100vw;
+  height: 100vh;
   z-index: 1;
-`;
-
-const OfferContainer = styled.div`
-  display: flex;
-  position: relative;
-  height: 30vh;
 `;
 
 const Bg2 = styled.img`
   display: flex;
-  position: relative;
-  width: 100%;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
   z-index: 1;
 `;
 
 const HowToUseContainer = styled.div`
   display: flex;
   position: relative;
-  height: 70vh;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const FooterContainer = styled(motion.div)`
+  display: flex;
+  position: relative;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
 function Landpage() {
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     axios.get('/login/check')
       .then((res) => {
@@ -63,29 +70,22 @@ function Landpage() {
   }, [navigate])
 
   return (
-  <>
-    <BgContainer>
-      <Bg1 src={landpageBg1}/>
-      <LandPageHeader />
-      <LandPageBody />
-    </BgContainer>
-    <OfferContainer>
-      <Bg2 src={landpageBg2}/>
-      <LandPageBody2 />
-    </OfferContainer>
-    <BgContainer>
-      <Bg1 src={landpageBg3} />
-      <LandPageBody3 />
-    </BgContainer>
-    <HowToUseContainer>
-      <Bg2 src={landpageBg4} />
-      <LandPageBody4 />
-    </HowToUseContainer>
-    <HowToUseContainer>
-        <Bg2 src={landPageBg5} />
-    </HowToUseContainer>
-  <Footer />
-  </>
+    <>
+    <LandingContainer>
+      <LandPageBody1 />
+      <BgContainer>
+        <Bg1 src={landpageBg3} />
+        <LandPageBody2 style={{ zIndex: '2'}}/>
+      </BgContainer>
+      <HowToUseContainer>
+        <Bg2 src={landPageBg4} />
+        <LandPageBody3 style={{ zIndex: '2' }} />
+      </HowToUseContainer>
+    </LandingContainer>
+    <FooterContainer>
+        <Footer />
+    </FooterContainer>
+    </>
   )
 }
 
